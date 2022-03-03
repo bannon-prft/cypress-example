fa75f4e9dffa421f8b54ba307cae0d16202ad370
Bannon Tanner <bannon.tanner@perficient.com>
2022-03-03 15:47:07 -0600

    all steps completed and tests working properly

--- cypress/integration/mySecondTest.js
+++ cypress/integration/mySecondTest.js
@@ -7,7 +7,14 @@ describe('JSON Placeholder Testing', () => {
         cy.request('GET', jsonJson.url).as('comments')
         cy.get('@comments').should(response => {
             expect(response.status).to.eq(200) // OK
-            
+
+            const firstComment = response.body[0]
+            expect(firstComment.postId).to.eq(jsonJson.postId)
+            expect(firstComment.id).to.eq(jsonJson.id)
+            expect(firstComment.name).to.eq(jsonJson.name)
+            expect(firstComment.email).to.eq(jsonJson.email)
+            expect(firstComment.body).to.eq(jsonJson.body)
+
             expect(response.body).to.have.length(500)
         })
     })
